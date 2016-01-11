bfff735d0abaf37607efd91be3ef8afd396c7d31
Jackong <JackongC@gmail.com>
2016-01-11 14:27:44 +0800

    add simple mocker for gin

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # gin-test
 a library for to test gin
+
+## Examples
+
+```go
+//init
+var mocker *mocker.Mocker
+var api *sling.Sling
+
+func init()  {
+	router := gin.Default()
+	group := router.Group("/")
+	group.Use(Discovery())
+	{
+		app.Route(group)
+	}
+
+	mocker = mocker.New(router)
+  api = sling.New()
+}
+
+//test
+body := Body{}
+req, _ := api.Get("/api/test").Request()
+res := mocker.Mock(req, &body)
+Expect(req.Method).To(Equal("GET"))
+Expect(res.Code).To(Equal(http.StatusOK))
+Expect(body.Code).To(Equal(Code.OK))
+```
---
+++ mocker/mocker.go
@@ -0,0 +1,33 @@
+package mocker
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Mocker define router and api to test http server with gin
+type Mocker struct {
+	Router *gin.Engine
+}
+
+//New a Mocker instance
+func New(router *gin.Engine) *Mocker {
+	if router == nil {
+		router = gin.Default()
+	}
+	gin.SetMode(gin.TestMode)
+	return &Mocker{Router: router}
+}
+
+//Mock gin http test
+func (mocker *Mocker) Mock(req *http.Request, body interface{}) (res *httptest.ResponseRecorder) {
+	res = httptest.NewRecorder()
+	mocker.Router.ServeHTTP(res, req)
+	if body != nil {
+		json.NewDecoder(res.Body).Decode(body)
+	}
+	return
+}
